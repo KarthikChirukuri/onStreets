103dc05f97c8037d2cfeb04fb620d0ef02007a46
KarthikChirukuri <karthik.chirukuri2005@gmail.com>
2026-05-02 22:01:56 +0530

    Started Adding JWT service Not implemented full YET

---
+++ backend/src/main/java/com/onStreets/entity/User.java
@@ -0,0 +1,22 @@
+package com.onStreets.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role; 
+}
